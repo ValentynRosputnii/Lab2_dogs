5d828d0eaacc7f5148960d1f15af603a1d93353e
ValentynRosputnii <61357425+ValentynRosputnii@users.noreply.github.com>
2021-12-05 13:40:33 +0200

    Lab2

---
+++ Lab_dogs/app/src/main/java/com/example/dogs_app/data/DataProvider.kt
@@ -0,0 +1,105 @@
+package com.example.dogs_app.data
+
+import com.example.dogs_app.R
+
+object DataProvider {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 1,
+            title = "Abby",
+            sex = "Female",
+            age = 7,
+            description = "Abby is a super loving dog who wants to seek out human companionship at all times of the day.",
+            puppyImageId = R.drawable.p1
+        ),
+        Puppy(
+            id = 2,
+            title = "Baxter",
+            sex = "Male",
+            age = 9,
+            description = "Baxter enjoys being the center of attention and likes to entertain his people.",
+            puppyImageId = R.drawable.p2
+        ),
+        Puppy(
+            id = 3,
+            title = "Fiona",
+            sex = "Female",
+            age = 11,
+            description = "Fiona is calm, confident, and courageous.",
+            puppyImageId = R.drawable.p3
+        ),
+        Puppy(
+            id = 4,
+            title = "Boomer",
+            sex = "Male",
+            age = 3,
+            description = "Boomer is great hunting and working companion if you live in the countryside, but he is also great and affectionate at home where he likes to snuggle and relax.",
+            puppyImageId = R.drawable.p4
+        ),
+        Puppy(
+            id = 5,
+            title = "Eden",
+            sex = "Female",
+            age = 1,
+            description = "Eden have an incredibly strong prey and hunting drive, and this is where that training will come in handy.",
+            puppyImageId = R.drawable.p5
+        ),
+        Puppy(
+            id = 6,
+            title = "Gunner",
+            sex = "Male",
+            age = 2,
+            description = "He's aloof toward strangers, although he should never be shy or aggressive, and highly territorial.",
+            puppyImageId = R.drawable.p6
+        ),
+        Puppy(
+            id = 7,
+            title = "Kali",
+            sex = "Female",
+            age = 14,
+            description = "The sweet, lovable and a true companion dog.",
+            puppyImageId = R.drawable.p7
+        ),
+        Puppy(
+            id = 8,
+            title = "Jax",
+            sex = "Male",
+            age = 9,
+            description = "He is alert, lively, and smart dog.",
+            puppyImageId = R.drawable.p8
+        ),
+        Puppy(
+            id = 9,
+            title = "Lizzy",
+            sex = "Female",
+            age = 7,
+            description = "Lizzy love to snuggle with her humans, and tend to stick closest to whoever her main caretaker is.",
+            puppyImageId = R.drawable.p9
+        ),
+        Puppy(
+            id = 10,
+            title = "Oakley",
+            sex = "Male",
+            age = 9,
+            description = "Oakley is fun-loving, playful, and affectionate, always ready for a rollicking good time.",
+            puppyImageId = R.drawable.p10
+        ),
+        Puppy(
+            id = 11,
+            title = "Taz",
+            sex = "Male",
+            age = 13,
+            description = "Taz is an intelligent dog who can make the ideal family pet if properly trained. ",
+            puppyImageId = R.drawable.p11
+        ),
+        Puppy(
+            id = 12,
+            title = "Wyatt",
+            sex = "Male",
+            age = 4,
+            description = "Wyatt is a calm dog who is aloof toward strangers but attentive toward their family.",
+            puppyImageId = R.drawable.p12
+        ),
+    )
+}
